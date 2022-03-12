c7cb5dbf1570ec502bd9a068bd37682d9819b10a
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2022-03-12 18:32:04 +0500

    fwef

--- SmartHR.Dashboard.Api/Startup.cs
+++ SmartHR.Dashboard.Api/Startup.cs
@@ -59,7 +59,7 @@ namespace SmartHR.Dashboard.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
