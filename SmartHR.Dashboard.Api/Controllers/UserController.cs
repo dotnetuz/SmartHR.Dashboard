46f237ab401c7c147287a0f548b2b86aee1ee105
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2022-03-12 16:41:33 +0500

    UoW, Repo, Service

---
+++ SmartHR.Dashboard.Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHR.Dashboard.Domain.Entities.User;
+using SmartHR.Dashboard.Service.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartHR.Dashboard.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create(User user)
+        {
+            var result = await _userService.CreateAsync(user);
+
+            return result.Error?.Code == 404 ? NotFound(result) : Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll(int pageSize, int pageIndex)
+            => Ok( await _userService.GetAllAsync(pageSize, pageIndex));
+    }
+}
---
+++ SmartHR.Dashboard.Domain/Common/ErrorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SmartHR.Dashboard.Domain.Common
+{
+    public class ErrorModel
+    {
+        public ErrorModel(int? code, string message = null)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public int? Code { get; set; }
+        public string Message { get; set; }
+    }
+}
